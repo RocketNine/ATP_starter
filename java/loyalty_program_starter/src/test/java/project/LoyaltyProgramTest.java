b5fdfab8119eea042db7ead74150a80e0a23e244
Cody Fletcher <cody@codyfletcher.com>
2018-09-25 11:39:24 -0700

    Add java/loyalty_program_starter project

---
+++ java/loyalty_program_starter/src/test/java/project/LoyaltyProgramTest.java
@@ -0,0 +1,14 @@
+package project;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoyaltyProgramTest {
+
+    @Test
+    public void TestSuiteWorking() {
+        Assert.assertTrue("JUnit test suite is working", true);
+    }
+
+}
