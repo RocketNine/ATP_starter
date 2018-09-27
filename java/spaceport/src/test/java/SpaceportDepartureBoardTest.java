0a07addafb5692b7924f7fe897b63ff707dd0dea
Paul Moore <pdmoore@gmail.com>
2018-09-27 08:39:47 -0700

    add recipe test to spaceport starter

--- java/spaceport/src/test/java/SpaceportDepartureBoardTest.java
+++ java/spaceport/src/test/java/SpaceportDepartureBoardTest.java
@@ -12,8 +12,15 @@ import static org.junit.Assert.fail;
 
 public class SpaceportDepartureBoardTest {
 
+    //TODO - Use the Stub Recipe to create a LaunchInfoProviderStub
     @Test
-    public void HowDoWeTestThisThing() {
-        fail("Uh oh");
+    public void LaunchesAreSortedByDestination_DestinationsAreUnique() {
+        // 1. Create LaunchInfoProviderStub
+
+        // 2/3. Create SUT - SpaceportDepartureBoard, using Constructor Injection
+
+        // 4. Exercise the SUT
+
+        // 5. Verify the results are sorted correctly
     }
 }
