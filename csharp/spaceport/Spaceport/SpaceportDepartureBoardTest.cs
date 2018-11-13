bc495420c01d585e06ae5902aadc3968a8e13e21
Paul Moore <pdmoore@gmail.com>
2018-11-13 13:07:49 -0800

    add recipe test to C# spaceport starter

--- csharp/spaceport/Spaceport/SpaceportDepartureBoardTest.cs
+++ csharp/spaceport/Spaceport/SpaceportDepartureBoardTest.cs
@@ -5,9 +5,18 @@ namespace csharp
     [TestFixture]
     public class SpaceportDepartureBoardTest
     {
+        
+        //TODO - Use the Stub Recipe to create a LaunchInfoProviderStub
         [Test]
         public void HowDoWeTestThisThing()
         {
+            // 1. Create LaunchInfoProviderStub (that implements ISpacelineLaunchInfoProvider)
+
+            // 2/3. Create SUT - SpaceportDepartureBoard, using Constructor Injection
+
+            // 4. Exercise the SUT
+
+            // 5. Verify the results are sorted correctly
             Assert.Fail("Uh oh");
         }
     }
