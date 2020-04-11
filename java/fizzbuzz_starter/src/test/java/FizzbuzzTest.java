f65c86453c8cae39fc49c2f01ce05540b3918d58
Paul Moore <pdmoore@gmail.com>
2020-04-10 21:16:37 -0700

    switch to JUnit 5.6.2

--- java/fizzbuzz_starter/src/test/java/FizzbuzzTest.java
+++ java/fizzbuzz_starter/src/test/java/FizzbuzzTest.java
@@ -1,6 +1,7 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FizzbuzzTest {
 
