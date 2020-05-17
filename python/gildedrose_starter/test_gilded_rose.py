46d57c3caae9287015958922b5bf32204ed44ca2
Cody Fletcher <cody@codyfletcher.com>
2020-05-17 13:49:06 -0700

    Add python/gildedrose_starter code with 1 failing test

---
+++ python/gildedrose_starter/test_gilded_rose.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from gilded_rose import Item, GildedRose
+
+
+class GildedRoseTest(unittest.TestCase):
+    def test_how_do_we_test_this_thing(self):
+        self.fail("Hooray! A failing test means our tests are running!")
+
+
+if __name__ == '__main__':
+    unittest.main()
