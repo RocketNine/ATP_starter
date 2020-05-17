b42670e2c7d0f65180ba3828d0ff00372f9d2383
Cody Fletcher <cody@codyfletcher.com>
2020-05-17 13:58:36 -0700

    Add python/stack_starter code with 1 failing test

---
+++ python/stack_starter/stack.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+class Stack(object):
+    pass
---
+++ python/stack_starter/test_stack.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from stack import Stack
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def test_how_do_we_test_this_thing(self):
+        self.fail("Hooray! A failing test means our tests are running!")
+
+
+if __name__ == '__main__':
+    unittest.main()
