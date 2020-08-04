97ab3498f37777bfb107b085ac96fcf1e4b5b363
Paul Moore <pdmoore@gmail.com>
2020-08-04 11:18:21 -0700

    spaceport steps numbered better

--- java/spaceport/src/test/java/com/rocketninesolutions/SpaceportDepartureBoardTest.java
+++ java/spaceport/src/test/java/com/rocketninesolutions/SpaceportDepartureBoardTest.java
@@ -10,13 +10,13 @@ public class SpaceportDepartureBoardTest {
     //TODO - Use the Stub Recipe to test that launches are sorted correctly
     @Test
     public void LaunchesAreSortedByDestination_DestinationsAreUnique() {
-        // 1. Create LaunchInfoProviderStub (that implements ISpacelineLaunchInfoProvider)
+        // Step 1. Create LaunchInfoProviderStub (that implements ISpacelineLaunchInfoProvider)
 
-        // 2/3. Create SUT - SpaceportDepartureBoard, using Constructor Injection
+        // Step 2 & 3. Create SUT - SpaceportDepartureBoard, using Constructor Injection
 
-        // 4. Exercise the SUT
+        // Step 4. Exercise the SUT
 
-        // 5. Verify the results are sorted correctly
+        // Step 5. Verify the results are sorted correctly
         fail("TODO - test the launch info sorting behavior");
     }
 }
