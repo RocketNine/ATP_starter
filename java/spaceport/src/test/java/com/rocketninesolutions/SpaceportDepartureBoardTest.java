dac7233cd035c2688cc297154c629ee0b8701849
Paul Moore <pdmoore@gmail.com>
2020-09-06 19:42:26 -0700

    placeholder test for start of exercise

--- java/spaceport/src/test/java/com/rocketninesolutions/SpaceportDepartureBoardTest.java
+++ java/spaceport/src/test/java/com/rocketninesolutions/SpaceportDepartureBoardTest.java
@@ -1,102 +1,20 @@
 package com.rocketninesolutions;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class SpaceportDepartureBoardTest {
 
     //TODO - Use the Stub Recipe to test that launches are sorted correctly
     @Test
-    public void Launched_WithinWindow_StillPartOfDepartureBoard() {
+    public void LaunchesAre_SortedByDestination_DestinationsAreUnique() {
+        // Step 1. Create LaunchInfoProviderStub (that implements ISpacelineLaunchInfoProvider)
 
-        LaunchInfo launch1 = new LaunchInfo(UUID.randomUUID());
-        launch1.setDestination("Mars");
-        launch1.setStatus(LaunchInfo.LaunchStatus.Launched);
-        launch1.setTime(LocalDateTime.now());
+        // Step 2 & 3. Create SUT - SpaceportDepartureBoard, using Constuctor Injection
 
-        MockProvider mock = new MockProvider();
-        mock.addLaunch(launch1);
+        // Step 4. Exercise the SUT
 
-        SpaceportDepartureBoard sut = new SpaceportDepartureBoard(mock);
-        sut.removeInactiveLaunches();
-
-        Assertions.assertEquals(1, sut.getLaunchList().size());
-    }
-
-    @Test
-    public void Scrubbed_WithinWindow_StillPartOfDepartureBoard() {
-
-        LaunchInfo launch1 = new LaunchInfo(UUID.randomUUID());
-        launch1.setDestination("Mars");
-        launch1.setStatus(LaunchInfo.LaunchStatus.Scrubbed);
-        launch1.setTime(LocalDateTime.now());
-
-        MockProvider mock = new MockProvider();
-        mock.addLaunch(launch1);
-
-        SpaceportDepartureBoard sut = new SpaceportDepartureBoard(mock);
-        sut.removeInactiveLaunches();
-
-        Assertions.assertEquals(1, sut.getLaunchList().size());
-    }
-
-    @Test
-    @Disabled
-    public void Launched_PastWindow_NotPartOfDepartureBoard() {
-
-        LaunchInfo launch1 = new LaunchInfo(UUID.randomUUID());
-        launch1.setDestination("Mars");
-        launch1.setStatus(LaunchInfo.LaunchStatus.Launched);
-        launch1.setTime(LocalDateTime.now());
-
-        MockProvider mock = new MockProvider();
-        mock.addLaunch(launch1);
-
-        SpaceportDepartureBoard sut = new SpaceportDepartureBoard(mock);
-
-        // mock something that can return the current time
-
-//        sut.setClock(mockClock);
-        sut.removeInactiveLaunches();
-
-        Assertions.assertEquals(0, sut.getLaunchList().size());
-
-    }
-
-    class MockProvider implements ISpacelineLaunchInfoProvider {
-
-        private List<LaunchInfo> launches;
-
-        public MockProvider() {
-            this.launches = new ArrayList<>();
-        }
-
-        @Override
-        public List<LaunchInfo> getCurrentLaunches() {
-            return launches;
-        }
-
-        @Override
-        public void addListener(LaunchInfoChangedListener p) {
-
-        }
-
-        @Override
-        public void removeListener(LaunchInfoChangedListener p) {
-
-        }
-
-        public void addLaunch(LaunchInfo launch) {
-            launches.add(launch);
-        }
+        // Step 5. Verify the results are sorted correctly
+        Assertions.fail("TODO - test the info sorting behavior");
     }
 }
