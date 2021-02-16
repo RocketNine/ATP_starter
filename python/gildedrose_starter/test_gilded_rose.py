2d2d15380bceb1bfbf750bcd01118bdee13e0138
Paul Moore <pdmoore@gmail.com>
2021-02-16 11:25:20 -0800

    python - use 'sut' for thing being tested

--- python/gildedrose_starter/test_gilded_rose.py
+++ python/gildedrose_starter/test_gilded_rose.py
@@ -8,10 +8,10 @@ class GildedRoseTest(unittest.TestCase):
     def test_needs_a_better_name(self):
         # Arrange
         items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+        sut = GildedRose(items)
 
         # Act
-        gilded_rose.update_quality()
+        sut.update_quality()
 
         # Assert
         self.assertEquals("fixme", items[0].name)
