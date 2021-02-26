79b626ddf1ec9e5f31a1e91d605795286d7bbd43
Paul Moore <pdmoore@gmail.com>
2021-02-26 13:09:17 -0800

    c# - add removeInactiveLaunches

--- csharp/spaceport/Spaceport/SpaceportDepartureBoard.cs
+++ csharp/spaceport/Spaceport/SpaceportDepartureBoard.cs
@@ -3,24 +3,44 @@ using System.Collections.Generic;
 
 namespace SpacePort
 {
-    public class SpaceportDepartureBoard : IObserver<LaunchInfo>
+    public class SpaceportDepartureBoard
     {
         private readonly ISpacelineLaunchInfoProvider _provider;
         
         public List<LaunchInfo> LaunchList { get; }
-
-        private Boolean _running;
         
         public SpaceportDepartureBoard(ISpacelineLaunchInfoProvider provider)
         {
             _provider  = provider;
             LaunchList = _provider.GetCurrentLaunches();
             SortLaunches();
-            _provider.Subscribe(this);
-            _running = true;
         }
 
-        public void OnNext(LaunchInfo info)
+        private void SortLaunches() {
+            throw new NotImplementedException("sorting logic hasn't been implemented yet");
+        }
+
+        protected void removeInactiveLaunches()
+        {
+            DateTime now = DateTime.Now;
+
+            foreach (LaunchInfo launchInfo in LaunchList)
+            {
+                if (launchInfo.Status == LaunchInfo.LaunchStatus.Launched)
+                {
+                    if (launchInfo.Time.AddMinutes(5).CompareTo(now) <= 0) {
+                        LaunchList.Remove(launchInfo);
+                    }
+                } else if (launchInfo.Status == LaunchInfo.LaunchStatus.Scrubbed)
+                {
+                    if (launchInfo.Time.AddMinutes(10).CompareTo(now) <= 0) {
+                        LaunchList.Remove(launchInfo);
+                    }
+                }
+            }
+        }
+        
+        public void OnLaunchInfoChanged(LaunchInfo info)
         {            
             if (info.Status == LaunchInfo.LaunchStatus.Delayed)
             {
@@ -33,10 +53,6 @@ namespace SpacePort
             {
                 AddNewLaunch(info);
             }
-        }
-
-        private void SortLaunches() {
-            throw new NotImplementedException("sorting logic hasn't been implemented yet");
         }
 
         private void AddNewLaunch(LaunchInfo info)
@@ -63,10 +79,5 @@ namespace SpacePort
         {
             throw new NotImplementedException();
         }
-
-        public void ShutDown()
-        {
-            _running = false;
-        }        
     }
 }
