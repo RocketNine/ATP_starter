07d820907106c60de6f24c4c64216b5b9cc083a8
Paul Moore <pdmoore@gmail.com>
2021-04-06 10:31:37 -0700

    python - add roman numerals starter

---
+++ python/roman_numerals/test_romannumerals.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from romannumerals import romannumerals
+
+
+class RomanNumeralsTest(unittest.TestCase):
+    def test_verify_test_framework_runs(self):
+        self.fail("Hooray! A failing test means our tests are running!")
+
+
+if __name__ == '__main__':
+    unittest.main()
