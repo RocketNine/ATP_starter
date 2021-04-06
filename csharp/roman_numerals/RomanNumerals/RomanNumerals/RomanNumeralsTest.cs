c5a7a89003636e7874c23cb16d1c1d7fd60e9e85
Paul Moore <pdmoore@gmail.com>
2021-04-05 20:49:32 -0700

    c# roman numerals starter

---
+++ csharp/roman_numerals/RomanNumerals/RomanNumerals/RomanNumeralsTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace RomanNumerals
+{
+    [TestFixture]
+    public class RomanNumeralsTest
+    {
+        [Test]
+        public void VerifyTestFrameworkRuns()
+        {
+            Assert.Fail("Hooray! A failing test means our system is working!");
+        }
+    }
+}
