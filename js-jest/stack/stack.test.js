dfedd82586d6d78c3baba29b022902bc2505bda8
Paul Moore <pdmoore@gmail.com>
2021-04-05 20:26:20 -0700

    js-jest comment out reference to stack

--- js-jest/stack/stack.test.js
+++ js-jest/stack/stack.test.js
@@ -1,4 +1,4 @@
-const stack = require('./stack');
+//const stack = require('./stack');
 
 test('verify test framework runs', () => {
     throw new Error('A failing test confirms the test framework is running!');
