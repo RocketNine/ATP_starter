31dde322c80d6ec8c73f8b7312745520a3ca0131
Paul Moore <pdmoore@gmail.com>
2021-04-05 20:27:56 -0700

    js-jest add roman numerals starting point

---
+++ js-jest/roman_numerals/roman_numerals.test.js
@@ -0,0 +1,6 @@
+//const roman_numerals = require('./roman_numerals');
+
+test('verify test framework runs', () => {
+    throw new Error('A failing test confirms the test framework is running!');
+});
+
