34f67c943a2c87946c8568978747d22b8fc5b9a5
Paul Moore <pdmoore@gmail.com>
2021-04-05 20:35:55 -0700

    java roman numerals starting point

---
+++ java/roman_numerals/src/test/java/RomanNumeralsTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RomanNumeralsTest {
+
+    @Test
+    public void verifyTestFrameworkRuns() {
+        fail("A failing test confirms the test framework is running!");
+    }
+
+}
