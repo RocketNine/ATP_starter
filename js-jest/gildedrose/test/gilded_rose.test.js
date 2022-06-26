7fe32ba9369fde43addacd6a05eb7c3b6ddd2b13
Paul Moore <pdmoore@gmail.com>
2022-06-26 09:27:00 -0700

    adjust js-jest test to be more exemplary (AAA; anonymous function)

--- js-jest/gildedrose/test/gilded_rose.test.js
+++ js-jest/gildedrose/test/gilded_rose.test.js
@@ -1,9 +1,14 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should DESCRIBE WHAT BEHAVIOR IS BEING CONFIRMED", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
+  it("should DESCRIBE WHAT BEHAVIOR IS BEING CONFIRMED", () => {
+    // Arrange
+    const sut = new Shop([new Item("foo", 0, 0)]);
+
+    // Act
+    const items = sut.updateQuality();
+
+    // Assert
     expect(items[0].name).toBe("fixme");
   });
 });
