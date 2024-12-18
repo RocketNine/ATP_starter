583d45248580ba511a4721c471f7afcb68d5c4ae
Paul Moore <pdmoore@gmail.com>
2024-12-17 17:47:12 -0800

    remove unneeded self in call to sort_launches

--- python/spaceport/spaceport_departure_board.py
+++ python/spaceport/spaceport_departure_board.py
@@ -1,7 +1,7 @@
 class SpaceportDepartureBoard():
     def __init__(self, launch_info_provider):
         self.launch_list = launch_info_provider.get_current_launches()
-        self.sort_launches(self)
+        self.sort_launches()
 
     def get_launch_list(self):
         return self.launch_list
